c68048b8ac8fb082d021c3b08a2fe29d2af9e919
subastianSk <subastian.sk@gmail.com>
2022-03-26 09:31:29 +0700

    Release 1

---
+++ Quiz/app/Controllers/Http/GenresController.ts
@@ -0,0 +1,61 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+
+export default class GenresController {
+  public async index({ request, response }: HttpContextContract) {
+    if (request.qs().name) {
+      const genreName = await Database.from('genres').where('name', request.qs().name)
+      response.ok({ message: 'success', data: genreName })
+    } else {
+      const genres = await Database.from('genres').select('*')
+      response.ok({ message: 'success', data: genres })
+    }
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const storeSchema = schema.create({
+      name: schema.string({}, [
+        rules.unique({ table: 'genres', column: 'name' }),
+        rules.minLength(4)
+      ]),
+    })
+    try {
+      await request.validate({ schema: storeSchema })
+      await Database.table('genres').returning('id').insert({
+        name: request.input('name'),
+      })
+      response.created({ message: 'Data stored' })
+    } catch (err) {
+      response.badRequest({ message: 'failed', error: err.messages })
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      const genres = await Database.from('genres').where('id', params.id).firstOrFail()
+      response.ok({ message: 'success!', data: genres })
+    } catch (err) {
+      response.notFound({ message: 'failed', error: err.message })
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const updateSchema = schema.create({
+      name: schema.string({}, [
+        rules.unique({ table: 'genres', column: 'name' }),
+        rules.minLength(4)
+      ]),
+    })
+    await request.validate({ schema: updateSchema })
+    await Database.from('genres').where('id', params.id).update({
+      name: request.input('name'),
+    })
+    response.ok({ message: 'data updated' })
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    await Database.from('genres').where('id', params.id).delete()
+    response.ok({ message: "Data deleted" })
+  }
+}
